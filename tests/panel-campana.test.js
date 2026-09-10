8281da6436fa8af0429178a82c64e0bc85c2b6bd
Santiago <santiafanador24@gmail.com>
2026-09-10 01:55:36 -0500

    feat: derive polling station metrics from volunteers

--- tests/panel-campana.test.js
+++ tests/panel-campana.test.js
@@ -25,5 +25,7 @@ assert.match(html, /data-demo-action="reference-toggle"/);
 assert.match(html, /showReferenceStations/);
 assert.match(html, /stationGoal/);
 assert.match(html, /\.map-leaflet\{[^}]*z-index:0[^}]*\}/);
+assert.match(html, /function buildVolunteerRegistry\(/);
+assert.match(html, /const volunteerRegistry =/);
 
 console.log('Panel de campaña: navegación y vistas mock presentes.');
